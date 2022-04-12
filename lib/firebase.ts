ff0aae6fff13e34bfe8ffc40a91c6efd28f35318
Yi Fan <yifan.g@saltmine.com>
2022-04-12 17:26:09 +0800

    feat: switch to npm and add firebase lib

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,7 +4,13 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/storage'
 
 const firebaseConfig = {
-    // your config
+  apiKey: "AIzaSyAcqewaCq0quEmbiKsPwEPnMpxNob6WiGw",
+  authDomain: "nextfire-app-cb45b.firebaseapp.com",
+  projectId: "nextfire-app-cb45b",
+  storageBucket: "nextfire-app-cb45b.appspot.com",
+  messagingSenderId: "607272118956",
+  appId: "1:607272118956:web:27898b8f5dae879abd0d09",
+  measurementId: "G-FMVQL4BSRC"
 }
   
 if (!firebase.apps.length) {
