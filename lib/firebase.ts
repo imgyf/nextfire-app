0b9fe5708938b1d49e3212e8b1ed4ef9a59aac35
Yi Fan <yifan.g@saltmine.com>
2022-04-15 18:36:11 +0800

    feat: add google sign in

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -20,3 +20,5 @@ if (!firebase.apps.length) {
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 export const storage = firebase.storage()
+
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
